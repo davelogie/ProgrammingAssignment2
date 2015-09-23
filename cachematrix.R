cc36b50b278b06ab5c1b6234ccd1f39e6af08865
Dave Logie <david.f.logie@gmail.com>
2015-09-23 17:41:20 -0400

    Added the necessary comments for the programming assignment
    Also testing the commit process

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Functions for programming assignment 2 which 
+##    a) Cache the Mean of a Vector
+##    b) Cache the Inverse of a Matrix
+
 ## Write a short comment describing this function
+
+## ** Cache the Mean of a Vector
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +15,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## ** Cache the Inverse of a Matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
